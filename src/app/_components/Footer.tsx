2c9ec8913932c01eb9865577bb4a08e0cf2c3988
Mohammed Meraj <105397144+MohammedMeraj@users.noreply.github.com>
2024-09-12 01:04:21 +0530

    Update Footer.tsx

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
     <div className='bg-gradient-to-r from-white to-gray-100 '>
         <div className='bg-gradient-to-t from-gray-900 to-black rounded-tl-lg rounded-tr-lg p-10 flex gap-6 text-gray-200 text-sm sm:text-md sm:flex-row flex-col justify-between tracking-wider ' >
         <div className='px-8'>
-            <div><Image src='/logo.svg' alt='logo' width={200} height={200}/></div>
+            <div><Image src='/logoW.svg' alt='logo' width={200} height={200}/></div>
             <div className='mt-2'>Empowering Startups and Investors to create <br/>game changing success stories together</div>
         </div>
         <div className='px-8'>
