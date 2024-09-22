043f202b92976f219c3522d62396a2d77b35e2ee
Mohammed Meraj <105397144+MohammedMeraj@users.noreply.github.com>
2024-09-22 22:34:44 +0530

    Dynamic Routing added

---
+++ src/app/(routes)/dashboard/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { Button } from '@/components/ui/button';
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import React, { useEffect } from 'react'
+
+function page() {
+
+  const {user} = useKindeBrowserClient();
+
+  useEffect(()=>{console.log("--",user)},[user])
+  return (
+    <div>
+<div>The Dashboard page</div>
+   <Button>
+   <LogoutLink>LogOut</LogoutLink>
+   </Button>
+
+    </div>
+    
+
+  )
+}
+
+export default page
