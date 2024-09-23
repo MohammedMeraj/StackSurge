091cdaa51e4ede7d5a9d836f4868dd2273785f4f
Meraj <mdmomin7517@gmail.com>
2024-09-23 20:19:26 +0530

    Dashboard page added

---
+++ src/app/(routes)/dashboard/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { LoginLink, LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
+import React from 'react'
+
+function Page() {
+  return (
+    <div className='w-screen  flex flex-col justify-center items-center h-screen'>
+        <div className='mb-5'>  Welcome to Dashboard !</div>
+          <Button>
+           <LogoutLink>
+            LogOut
+           </LogoutLink>
+           </Button>
+        
+        
+       
+    </div>
+  )
+}
+
+export default Page
