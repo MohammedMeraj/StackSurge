ef10c6b64f3d87a6589ff1b5eb6ca3d92be65630
Meraj <mdmomin7517@gmail.com>
2024-09-23 23:08:52 +0530

    Added Dashbord Page

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Button } from '@/components/ui/button'
-import { LoginLink, LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
 import React from 'react'
 
 function Page() {
