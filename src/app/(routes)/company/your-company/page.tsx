eb0ce52dd4743373b769b702bc066157f9d1ba8e
MohammedMeraj <mdmomin7517@gmail.com>
2024-10-16 00:07:08 +0530

    form added, middleware changed

--- src/app/(routes)/company/your-company/page.tsx
+++ src/app/(routes)/company/your-company/page.tsx
@@ -7,15 +7,57 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage} from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+
 
 const formSchema = z.object({
-    companyName: z.string().min(2, {
-      message: "Username must be at least 2 characters.",
+    companyName: z.string().min(3, {
+      message: "Company Name must be at least 3 characters.",
     }),
-    description: z.string().min(20, {
-      message: "Username must be at least 20 characters.",
+    description: z.string().min(50, {
+      message: "Company Description must be at least 50 characters.",
     }),
+    website: z.string().min(4, {
+      message: "Company Description must be at least 4 characters.",
+    }),
+    companyEmail: z.string().min(5, {
+      message: "Company Email must be at least 5 characters.",
+    }),
+    grossMargin: z.string().min(1, {
+      message: "At leaststring 1 characters.",
+    }),
+    netProfitMargin: z.string().min(1, {
+      message: "At least 1 characters.",
+    }),
+    operatingMargin: z.string().min(1, {
+      message: "At least 1 characters.",
+    }),
+    freeCashFlow: z.string().min(1, {
+      message: "At least 1 characters.",
+    }),
+
+
+    burnRate: z.string().min(1, {
+      message: "At least 1 characters.",
+    }),
+    latestValuation: z.string().min(1, {
+      message: "At least 1 characters.",
+    }),
+    ebitda: z.string().min(4, {
+      message: "At least 1 characters.",
+    }),
+    projectedValuation: z.string().min(1, {
+      message: "At least 1 characters.",
+    }),
+
+    currentRevenue: z.string().min(1, {
+      message: "At least 1 characters.",
+    }),
+    revenueIncreased: z.string().min(1, {
+      message: "At least 1 characters.",
+    }),
+   
     
   })
 
@@ -43,7 +85,21 @@ const Page = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       companyName: "",
-      description: ""
+      description: "",
+      website: "",
+      companyEmail: "",
+      grossMargin: "",
+      netProfitMargin: "",
+      operatingMargin: "",
+      freeCashFlow: "",
+      burnRate: "",
+      latestValuation: "",
+      ebitda: "",
+      projectedValuation: "",
+      currentRevenue: "",
+      revenueIncreased: ""
+
+      
     },
   })
  
@@ -55,8 +111,9 @@ const Page = () => {
   }
 
   return (
-    <div>
-      <form
+    <div className="max-w-7xl ml-auto mr-auto">
+    <div className='pl-7 pr-7 pt-7 flex flex-col justify-center items-center' >
+      <form className="hidden"
         onSubmit={(e) => {
           e.preventDefault();
           create();
@@ -82,8 +139,10 @@ const Page = () => {
 
     
 
-
-      <div className="w-[70%] ml-[15%] mr-[15%]  mt-20   ">
+      <div className='text-xl self-start font-bold mb-8 text-gray-900'>Publish Your Company</div>
+      
+      <div className="w-[95%] ml-[15%] mr-[15%] border p-7 rounded-md">
+      <div className="text-xl mb-7">Basic Details</div>
       <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
@@ -91,9 +150,9 @@ const Page = () => {
           name="companyName"
           render={({ field }) => (
             <FormItem>
-             
+             <FormLabel>Company Name</FormLabel>
               <FormControl>
-                <Input placeholder="Company" {...field} />
+                <Input placeholder="example : StackSurge" {...field} />
               </FormControl>
               <FormDescription>
                 Specify your company name
@@ -108,9 +167,9 @@ const Page = () => {
           name="description"
           render={({ field }) => (
             <FormItem>
-              
+              <FormLabel>Company Description</FormLabel>
               <FormControl>
-                <Input placeholder="Description" {...field} />
+                <Input placeholder="example : Investor and Startup Association Platform" {...field} />
               </FormControl>
               <FormDescription>
                 Provide company description
@@ -120,10 +179,258 @@ const Page = () => {
             </FormItem>
           )}
         />
+         <FormField
+          control={form.control}
+          name="website"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Website</FormLabel>
+              <FormControl>
+                <Input placeholder="example : stack-surge.vercel.app" {...field} />
+              </FormControl>
+              <FormDescription>
+                Company&apos;s official website
+              </FormDescription>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="companyEmail"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="example : email@stacksurge.com" {...field} />
+              </FormControl>
+              <FormDescription>
+                Company&apos;s email address
+              </FormDescription>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+        <Separator/>
+        
+           {/*Profit Margin  &#40; Overall &#41; */}
+        <div className="text-xl">Profit Margin</div>
+              <div className="flex w-full justify-between gap-5" >
+             <FormField 
+        control={form.control}
+        name="grossMargin"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Gross Margin  &#40; &#36; &#41; </FormLabel>
+            <FormControl >
+              <Input type="number" placeholder="Enter Amount" {...field} />
+            </FormControl>
+            <FormDescription>
+              Company&apos;s Gross Margin
+            </FormDescription>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+              <FormField 
+        control={form.control}
+        name="netProfitMargin"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Net Profit Margin &#40; &#36; &#41;</FormLabel>
+            <FormControl >
+              <Input type="number" placeholder="Enter Amount" {...field} />
+            </FormControl>
+            <FormDescription>
+              Company&apos;s Net Profit Margin
+            </FormDescription>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+        <FormField 
+        control={form.control}
+        name="operatingMargin"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Operatig Margin &#40; &#36; &#41;</FormLabel>
+            <FormControl >
+              <Input type="number" placeholder="Enter Amount" {...field} />
+            </FormControl>
+            <FormDescription>
+              Company&apos;s Operatig Margin
+            </FormDescription>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+              </div>
+
+              <Separator/>
+
+
+            {/*Cash Flow  &#40; Overall &#41; */}
+              <div className="text-xl">Cash Flow</div>
+              <div className="flex w-full justify-between gap-5" >
+             <FormField 
+        control={form.control}
+        name="freeCashFlow"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Free Cash Flow  &#40; &#36; &#41; </FormLabel>
+            <FormControl >
+              <Input type="number" placeholder="Enter Amount" {...field} />
+            </FormControl>
+            <FormDescription>
+              Company&apos;s Free Cash Flow
+            </FormDescription>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+              <FormField 
+        control={form.control}
+        name="burnRate"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Burn Rate &#40; &#36; &#41;</FormLabel>
+            <FormControl >
+              <Input type="number" placeholder="Enter Amount" {...field} />
+            </FormControl>
+            <FormDescription>
+              Company&apos;s Burn Rate per month
+            </FormDescription>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+        
+              </div>
+
+              <Separator/>
+
+
+            {/*Valuation &#40; Overall &#41;*/}
+            <div className="text-xl">Company Valuation</div>
+              <div className="flex w-full justify-between gap-5" >
+             <FormField 
+        control={form.control}
+        name="latestValuation"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Latest valuation  &#40; &#36; &#41; </FormLabel>
+            <FormControl >
+              <Input type="number" placeholder="Enter Amount" {...field} />
+            </FormControl>
+            <FormDescription>
+              Company&apos;s Latest valuation
+            </FormDescription>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+              <FormField 
+        control={form.control}
+        name="ebitda"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>EBITDA &#40; &#36; &#41;</FormLabel>
+            <FormControl >
+              <Input type="number" placeholder="Enter Amount" {...field} />
+            </FormControl>
+            <FormDescription>
+              Company&apos;s EBITDA
+            </FormDescription>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+        <FormField 
+        control={form.control}
+        name="projectedValuation"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Projected Valuation &#40; &#36; &#41;</FormLabel>
+            <FormControl >
+              <Input type="number" placeholder="Enter Amount" {...field} />
+            </FormControl>
+            <FormDescription>
+              Company&apos;s Projected Valuation for next 3 years
+            </FormDescription>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+              </div>
+
+              <Separator/>
+
+              {/*Revenue Growth*/}
+              <div className="text-xl">Revenue Growth</div>
+              <div className="flex w-full justify-between gap-5" >
+             <FormField 
+        control={form.control}
+        name="currentRevenue"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Current Revenue &#40; &#36; &#41; </FormLabel>
+            <FormControl >
+              <Input type="number" placeholder="Enter Amount" {...field} />
+            </FormControl>
+            <FormDescription>
+              Company&apos;s Current Revenue
+            </FormDescription>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+              <FormField 
+        control={form.control}
+        name="revenueIncreased"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Revenue increased &#40; &#36; &#41;</FormLabel>
+            <FormControl >
+              <Input type="number" placeholder="Enter Amount" {...field} />
+            </FormControl>
+            <FormDescription>
+              Company&apos;s Revenue Increased in last one year
+            </FormDescription>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+        
+              </div>
+
+              <Separator/>
+
+
+
+
+
+
+              
+            
+        
         <Button type="submit" className="w-full">Submit</Button>
       </form>
     </Form>
    </div>
+    </div>
     </div>
   );
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -41,5 +41,5 @@ export async function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/dashboard', '/investor', '/select','/investor/profile'],
+  matcher: ['/company/:path*','/investor/:path*','/select','/test'],
 };
