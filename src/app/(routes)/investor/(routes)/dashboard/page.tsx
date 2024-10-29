72dfdc83c12246035af63fe4fa01b35c157ee62e
MohammedMeraj <mdmomin7517@gmail.com>
2024-10-30 00:10:14 +0530

    layout fix : Investor - Dashboard page

--- src/app/(routes)/investor/(routes)/dashboard/page.tsx
+++ src/app/(routes)/investor/(routes)/dashboard/page.tsx
@@ -5,14 +5,17 @@ import TrackList from '@/app/_components/TrackList'
 import { Factory, Wallet } from 'lucide-react'
 const Dashboard = () => {
   return (
-    <div className='w-full max-w-6xl  '>
+    <div className='mx-7  '>
       <div className="text-2xl px-8 self-start font-bold mb-1 mt-4 text-gray-900">
         Dashboard
       </div>
-      <div className='flex mx-7 h-fit items-start justify-between gap-7 mt-3'>
-          <div className='w-[60%] mt-6 '>
-          <div className='text-lg  font-bold'>Overview</div>
+      <div className=' mx-auto w-full max-w-6xl h-fit items-start justify-between gap-7 mt-3'>
+          <div className=' mt-6 '>
+            {/*
+            <div className='text-lg  font-bold'>Overview</div>
           <div className='text-sm mb-7'>A refined summary of essential data insights</div>
+            */}
+          
             <div className='w-[100%] flex items-center justify-center gap-5'>
               <div className='w-[50%] border rounded-md p-4 h-28'> 
                 <div className='flex items-start justify-between'>
@@ -36,16 +39,23 @@ const Dashboard = () => {
             
           </div>
           
-          <div className='w-[40%] border rounded-md p-5 mt-4'> 
+          <div className=' border rounded-md p-5 mt-4 hidden'> 
             <div className='text-lg  font-bold'>Recently Invested</div>
             <div className='text-sm mb-2'>Showing companies you recently Invested in.</div>
             <TrackList/> 
             </div>
+
+            <div className='flex w-full h-fit items-start justify-between gap-7 mt-7 mb-7'>
+          <div className='w-[50%]'> <LineChart/> </div>
+          <div className=' border w-[50%] rounded-md p-4'> 
+            <div className='text-lg  font-bold'>Recently Invested</div>
+            <div className='text-sm mb-2'>Showing companies you recently Invested in.</div>
+            <TrackList/> 
+            </div>
+           </div>
       </div>
-      <div className='flex mx-7 h-fit items-start justify-between gap-7 mt-7 mb-7'>
-          <div className='w-[57%]'> <LineChart/> </div>
-      </div>
-      <div></div>
+      
+      
       
     </div>
   )
