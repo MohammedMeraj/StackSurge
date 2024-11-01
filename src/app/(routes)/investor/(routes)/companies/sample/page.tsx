08408c23a87ea01a5c1d016896f4b881c3818a86
MohammedMeraj <mdmomin7517@gmail.com>
2024-11-02 00:31:29 +0530

    Payment Page : Debugged - iteration 1

--- src/app/(routes)/investor/(routes)/companies/sample/page.tsx
+++ src/app/(routes)/investor/(routes)/companies/sample/page.tsx
@@ -14,12 +14,13 @@ export default function Home() {
   const amount = 49.99;
 
   return (
-    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+  <div className="mx-7">
+    <div className="max-w-6xl mx-auto p-10 text-gray-800 text-center border m-10 rounded-md ">
       <div className="mb-10">
-        <h1 className="text-4xl font-extrabold mb-2">Sonny</h1>
-        <h2 className="text-2xl">
-          has requested
-          <span className="font-bold"> ${amount}</span>
+        <h1 className="text-2xl  mb-2">Make Payment </h1>
+        <h2 className="text-lg mt-6">
+          Investing &nbsp;&nbsp;
+          <span className="font-bold text-base"> ${amount}</span>
         </h2>
       </div>
 
@@ -31,8 +32,10 @@ export default function Home() {
           currency: "usd",
         }}
       >
+        
         <CheckoutPage amount={amount} />
       </Elements>
-    </main>
+    </div>
+    </div>
   );
 }
